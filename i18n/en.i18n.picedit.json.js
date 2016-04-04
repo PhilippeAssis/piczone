833b894095fd2c18d4212dce69678b50d1661f6d
Philippe Assis <assis@philippeassis.com>
2016-04-04 17:55:32 -0300

    Init

---
+++ i18n/en.i18n.picedit.json.js
@@ -0,0 +1,11 @@
+var PICEDIT_I18N = {
+    'en' : {
+        webcamError : "No video source detected! Please allow camera access!",
+        webRTCError: "Sorry, your browser doesn't support WebRTC!",
+        info: "or copy/paste image here",
+        imageEmpty: "Open an image or use your camera to make a photo!",
+        formDataError: "Sorry, the FormData API is not supported!",
+        dataSubmitSuccess : "Data successfully submitted!",
+        dataSubmitError : "Server did not accept data!"
+    }
+}
---
+++ i18n/ptbr.i18n.picedit.json.js
@@ -0,0 +1,11 @@
+var PICEDIT_I18N = {
+    'ptbr' : {
+        webcamError : "Nenhuma fonte de vídeo detectado! Por favor, permita o acesso câmera.",
+        webRTCError: "Desculpe, seu navegador não suporta WebRTC.",
+        info: "ou a imagem de copiar/colar aqui",
+        imageEmpty: "Abra uma imagem ou usar sua câmera para fazer uma foto!",
+        formDataError: "Lamentamos, mas o API FormData não é suportado!",
+        dataSubmitSuccess : "Dados enviado com sucesso!",
+        dataSubmitError : "O servidor não aceitou os dados enviados."
+    }
+}
